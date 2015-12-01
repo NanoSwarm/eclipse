477dad890a55defd8010b6eb95f28e2fe40ab155
nanoswarm <plp3a.nanoswarm@gmail.com>
2015-12-01 13:30:59 +0100

    comment added

--- src/swarm/model/level/room/UpdatePositionInRoom.java
+++ src/swarm/model/level/room/UpdatePositionInRoom.java
@@ -4,6 +4,12 @@ import swarm.model.SwarmParameters;
 import swarm.model.agents.Drone.room.AgtDronePLSInRoom;
 
 public class UpdatePositionInRoom {
+	
+	/**
+	 * the method updating the position of drones in the RoomLevel.
+	 * @param agtDrone drone send by RoomLevel for position update
+	 * @param parameters the parameters of the simulation
+	 */
 	public static void UpdateDronePosition(AgtDronePLSInRoom agtDrone, SwarmParameters parameters){
 		
 		agtDrone.setAcceleration(
@@ -37,6 +43,7 @@ public class UpdatePositionInRoom {
 					parameters.maxAcc * agtDrone.getAcceleration().z / acc);
 		}
 		
+		//Update position and speed by integration (of speed and acceleration)
 		agtDrone.setLocation(
 				agtDrone.getLocation().x + agtDrone.getVelocity().x + agtDrone.getAcceleration().x / 2, 
 				agtDrone.getLocation().y + agtDrone.getVelocity().y + agtDrone.getAcceleration().y / 2,
@@ -47,7 +54,8 @@ public class UpdatePositionInRoom {
 					agtDrone.getVelocity().y + agtDrone.getAcceleration().y,
 					agtDrone.getVelocity().z + agtDrone.getAcceleration().z
 			);
-		// Keep the Drones in the limits of the room
+		
+		// Keep the Drones in the limits of the room, temporary (before real limitation to the room.
 		if( agtDrone.getLocation().x > parameters.roomBounds.getWidth()) {
 			agtDrone.setLocation(0,agtDrone.getLocation().y,agtDrone.getLocation().z);
 		}
